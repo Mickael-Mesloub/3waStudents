a34df7ef7390d188b12376366ac8cc13b5f98f55
Mickael-Mesloub <mickael.mesloub@gmail.com>
2023-10-25 11:54:41 +0200

    feat: add terminal styling with chalk

---
+++ index.js
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+import { students } from "./students.js";
+
+console.log(chalk.blue(students.melissa.name));
+console.log(chalk.yellow(students.teddy.name));
+console.log(chalk.green(students.theo.name));
+console.log(chalk.red(students.thilbault.name));
---
+++ students.js
@@ -0,0 +1,14 @@
+export const students = {
+	teddy: {
+		name: "Teddy",
+	},
+	thilbault: {
+		name: "Thibault",
+	},
+	melissa: {
+		name: "Mélissa",
+	},
+	theo: {
+		name: "Théo",
+	},
+};
